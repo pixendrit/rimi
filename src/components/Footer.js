f54cfd5790d1ac60f00b4e0fe8122253ed4c6f9c
pixendrit <pixendrit@outlook.com>
2020-10-12 03:49:35 +0200

    danger

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,15 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      Na ndiqni {' '}
+      <a href="https://instagram.com/rimicom/">@rimicom</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © Copyright {new Date().getFullYear()} All rights reserved. Zhvilluar nga{' '}
+          <a href="https://pixendrit.com">Pixendrit</a>.
         </span>
       </div>
     </footer>
