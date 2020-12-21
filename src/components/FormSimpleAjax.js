80012a899ad2ca360dd56e607b780bdf4d702859
pixendrit <pixendrit@outlook.com>
2020-12-21 01:45:28 +0100

    nocaptcha

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -160,10 +160,6 @@ class Form extends React.Component {
             <span>Mesazhi juaj</span>
           </label>
        
-          <div
-            className="g-recaptcha"
-            data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
-          />
           {!!subject && <input type="hidden" name="subject" value={subject} />}
           <input type="hidden" name="form-name" value={name} />
           <input
